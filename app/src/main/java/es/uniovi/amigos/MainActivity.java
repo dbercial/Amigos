62793eb4a5c9382fc5d4965c71fa9994fb315b19
dbercial <dberciala@gmail.com>
2024-01-09 22:44:47 +0100

    Initial commit

---
+++ app/src/main/java/es/uniovi/amigos/MainActivity.java
@@ -0,0 +1,92 @@
+package es.uniovi.amigos;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+    private MapView map = null;  // Este atributo guarda una referencia al objeto MapView
+    // a través del cual podremos manipular el mapa que se muestre
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Leer la configuración de la aplicación
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        // Crear el mapa desde el layout y asignarle la fuente de la que descargará las imágenes del mapa
+        setContentView(R.layout.activity_main);
+        map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        // Solicitar al usuario los permisos "peligrosos". El usuario debe autorizarlos
+        // Cuando los autorice, Android llamará a la función onRequestPermissionsResult
+        // que implementamos más adelante
+        requestPermissionsIfNecessary(new String[]{
+                // WRITE_EXTERNAL_STORAGE este permiso es necesario para guardar las imagenes del mapa
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // Esta función será invocada cuando el usuario conceda los permisos
+        // De momento hay que dejarla como está
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (int i = 0; i < grantResults.length; i++) {
+            permissionsToRequest.add(permissions[i]);
+        }
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+    private void requestPermissionsIfNecessary(String[] permissions) {
+        // Itera por la lista de permisos y los va solicitando de uno en uno
+        // a menos que estén ya concedidos (de ejecuciones previas)
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted
+                permissionsToRequest.add(permission);
+            }
+        }
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+}
